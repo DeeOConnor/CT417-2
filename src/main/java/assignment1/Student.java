8136a7dc5b5c7dbf1d4b01318565d5a8a5620939
15332286 <15332286@NUIGIT558.it.nuigalway.ie>
2018-10-19 11:51:03 +0100

    Trying to fix LocalDate problem with dob in student class

--- src/main/java/assignment1/Student.java
+++ src/main/java/assignment1/Student.java
@@ -2,13 +2,14 @@ package assignment1;
 
 import java.time.LocalDate;
 import java.time.Period;
+import static org.joda.time.DateTimeFieldType.year;
 //@author 15332286
 
 public class Student {
 
     private String name;
     private int age;
-    private LocalDate dob;
+    private LocalDate dob; //= format what.. to pull out specific year
     private LocalDate today;
     private static int ID = 201800;
 
@@ -19,7 +20,7 @@ public class Student {
     //https://stackoverflow.com/questions/29062204/create-localdate-object-from-integers
     public Student(String name, LocalDate dob, Course course, Module module) {
         this.name = name;
-        this.dob = dob.of(year, month, day);
+        this.dob = dob.of(year);
         this.course = course;
         this.module = module;
         this.ID +=1;
@@ -44,7 +45,7 @@ public class Student {
     public int getAge() {
         return age;
     }
-    public Date getDOB() {
+    public LocalDate getDOB() {
         return dob;
     }
     
