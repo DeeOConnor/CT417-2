471d8e17e74135e38f3ce71b1b62e67ea8fc8ba0
dee1o <dee1o@192.168.0.228>
2018-10-24 20:34:32 +0100

    assignment 1 part two initial commit with skeleton classes and pom made

---
+++ src/main/java/com/mycompany/assignment1_part2/MockUniversity.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.assignment1_part2;
+
+/**
+ *
+ * @author dee1o
+ */
+public class MockUniversity {
+    //make students here and test the data!
+}
