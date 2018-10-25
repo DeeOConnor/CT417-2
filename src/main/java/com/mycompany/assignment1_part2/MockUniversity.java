faaebd27c3a32818b8c4ccfd0c3f6a3e6354965a
dee1o <dee1o@192.168.0.228>
2018-10-25 18:56:19 +0100

    added content to mockUniversity class

--- src/main/java/com/mycompany/assignment1_part2/MockUniversity.java
+++ src/main/java/com/mycompany/assignment1_part2/MockUniversity.java
@@ -23,6 +23,29 @@ public class MockUniversity {
     
     Course c1 = new Course("CS", start, end);
     
+    //MODULES CT417, CT418, Maths, CS150, FYP
+    //Modules have name and String ID
+    Module ct417 = new Module("CT417", "CT417");
+    Module ct418 = new Module("CT418", "CT418");
+    Module maths = new Module("Maths", "Maths");
+    Module cs150 = new Module("CS150", "CS150");
+    Module fyp = new Module("FYP", "FYP");
+    
+    //students have name course module
+    //Students should be able to have many modules
+    //List of modules should use .add(student)
+    Student s1 = new Student("Anna", "CS", "CT417");
+    Student s2 = new Student("Barry", "CS", "Maths");
+    Student s3 = new Student("Carla", "CS", "Maths");
+    Student s4 = new Student("Darren", "CS", "CT418");
+    Student s5 = new Student("Edel", "CS", "CS150");
+    Student s6 = new Student("Fergus", "CS", "CS150");
+    Student s7 = new Student("Grainne", "CS", "CT417");
+    Student s8 = new Student("Harry", "CS", "CT417");
+    Student s9 = new Student("Ida", "CS", "CS150");
+    Student s10 = new Student("Joe", "CS", "CT418");
+       
+    
     //lots of modules, have lots of students
     //create lots of students
     //print out a list of all  courses, modules, and students, usernames, assigned modules and registered courses 
