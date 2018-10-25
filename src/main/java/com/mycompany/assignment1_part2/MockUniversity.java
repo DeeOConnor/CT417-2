b95bdff062d905b831d5cbf7199e3bd4a1e6ef95
dee1o <dee1o@192.168.0.228>
2018-10-25 18:49:07 +0100

    added content to mockUniversity class

--- src/main/java/com/mycompany/assignment1_part2/MockUniversity.java
+++ src/main/java/com/mycompany/assignment1_part2/MockUniversity.java
@@ -5,10 +5,26 @@
  */
 package com.mycompany.assignment1_part2;
 
+import assignment1.Course;
+import assignment1.Module;
+import assignment1.Student;
+import org.joda.time.DateTime;
+
+
+
 /**
  *
- * @author dee1o
+ * @author 15332286
  */
 public class MockUniversity {
     //make students here and test the data!
+    DateTime start = new DateTime();
+    DateTime end = new DateTime();
+    
+    Course c1 = new Course("CS", start, end);
+    
+    //lots of modules, have lots of students
+    //create lots of students
+    //print out a list of all  courses, modules, and students, usernames, assigned modules and registered courses 
+    
 }
